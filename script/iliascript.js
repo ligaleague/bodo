6b6f9299b671d3940d8a95c8170ac34001c7a0e7
starmus <ilusha.petrosian@gmail.com>
2022-04-13 22:30:35 +0400

    dsds

--- script/iliascript.js
+++ script/iliascript.js
@@ -22,4 +22,17 @@ const myObserver = new IntersectionObserver(callback, options)
 myObserver.observe(img)
 myObserver.observe(img1)
 
-console.log("123");
+// setTimeout(function(){
+//     let div=document.getElementById("animtexttop");
+//     div.style.transform="translateX(0px)";
+//     console.log(div);
+// }, 1000);
+
+let div=document.getElementById("animtexttop");
+div.style.transform="translateX(0px)";
+
+let div1=document.getElementById("animtextmiddle");
+div1.style.transform="translateX(0px)";
+
+let div2=document.getElementById("animtextbot");
+div2.style.transform="translateX(0px)";
