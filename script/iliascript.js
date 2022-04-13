c9297a8972114e98fb9390dd8b545160f789e0f7
starmus <ilusha.petrosian@gmail.com>
2022-04-13 19:45:45 +0400

    observer

---
+++ script/iliascript.js
@@ -0,0 +1,25 @@
+const img = document.querySelector(".left")
+const img1 = document.querySelector(".right")
+
+
+const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fadeIn")
+        }
+        else {
+            entry.target.classList.remove("fadeIn")
+        }
+    })
+}
+
+const options = {
+    threshold: 0.5,
+}
+
+const myObserver = new IntersectionObserver(callback, options)
+
+myObserver.observe(img)
+myObserver.observe(img1)
+
+console.log("123");
